7b524dc6d4a6c3e8797fab63992fde83219f75b4
zhuck96 <zhuck9615@yandex.ru>
2025-08-23 16:15:00 +0300

    update playwright events pep-8

--- playwright_events.py
+++ playwright_events.py
@@ -1,13 +1,13 @@
-from playwright.sync_api import sync_playwright,Request, Response
+from playwright.sync_api import sync_playwright, Request, Response
 
 # работа с событиями - просмотр запросов отправляемых странцией и полученных кодов ответов по ним
 
 # можно реализовать как отдельными функциями там и через lambda
 
-# def log_request(request:Request):
+# def log_request(request: Request):
 #     print(f"Запрос {request.url}")
 #
-# def log_response(response:Response):
+# def log_response(response: Response):
 #     print(f"Ответ {response.url} {response.status}")
 
 with sync_playwright() as playwright:
@@ -16,8 +16,8 @@ with sync_playwright() as playwright:
     page = context.new_page()
     # page.on("request", log_request)
     # page.on("response", log_response)
-    page.on("request",lambda request: print(request.url))
-    page.on("response",lambda response: print(response.url, response.status))
+    page.on("request", lambda request: print(request.url))
+    page.on("response", lambda response: print(response.url, response.status))
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
     page.wait_for_timeout(5000)
