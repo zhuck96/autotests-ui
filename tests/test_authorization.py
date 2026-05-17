4184258d1be2d521ae1553c059b0cac5d8f03822
zhuck96 <zhuck9615@yandex.ru>
2026-05-17 14:36:44 +0300

    add_marks

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,8 @@
 from playwright.sync_api import sync_playwright, expect
+import pytest
 
+@pytest.mark.regression
+@pytest.mark.authorization
 def test_wrong_email_or_password_authorization():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
